fcb18000dba2690999d753ef0f1593550a0ea29c
tryuhowcollect <t.ryu.howcollect@gmail.com>
2024-05-07 23:49:54 +0900

    fix:ip

---
+++ Master-Controller/data/settings.py
@@ -0,0 +1,19 @@
+# public IP address through the which it is possible to access thingvisors, database, vSilos, etc.
+default_gateway_IP = "127.0.0.1"
+
+# MQTT settings (possibly two brokers one for control and another one for control messages)
+MQTT_data_broker_IP = "127.0.0.1"
+MQTT_data_broker_port = 1883
+MQTT_control_broker_IP = "127.0.0.1"
+MQTT_control_broker_port = 1883
+
+# Mongo settings
+# mongoDb port
+# mongo_IP is required but can be empty !!
+mongo_IP = "127.0.0.1"
+mongo_port = 27017
+
+container_manager = "DOCKER"
+
+#Authentication settings
+JWT_SECRET_KEY = "UYG867ti867f(/&$SWRUco)(YPO/T"  # secret key used to encrypt password
