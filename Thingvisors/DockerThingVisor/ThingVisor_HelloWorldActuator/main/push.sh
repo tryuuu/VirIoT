6d5fcb67406316ba2e18282ef18adec94165fc96
tryuuu <ryu23210@gmail.com>
2024-12-07 12:12:39 +0900

    ローカル動作確認まで

--- Thingvisors/DockerThingVisor/ThingVisor_HelloWorldActuator/main/push.sh
+++ Thingvisors/DockerThingVisor/ThingVisor_HelloWorldActuator/main/push.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo docker login
-sudo docker build -t tryuu/thingvisor_helloworld_actuator_main:hello .
-sudo docker push tryuu/thingvisor_helloworld_actuator_main:hello
+sudo docker build -t tryuu/thingvisor_helloworld_actuator_main:latest .
+sudo docker push tryuu/thingvisor_helloworld_actuator_main:latest
